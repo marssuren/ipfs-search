60c2d1467ff939f29bb9ef95b68589c4d36cb998
marssuren <287815741@qq.com>
2025-03-14 02:50:38 +0800

    添加注释

--- components/sniffer/proxy/proxy.go
+++ components/sniffer/proxy/proxy.go
@@ -6,11 +6,14 @@ import (
 	"github.com/ipfs/go-datastore"
 )
 
-// New wraps a datastore in a proxy calling afterPut after every Put() operation.
+// New 将一个数据存储包装在一个代理中，并在每次 Put() 操作之后调用 afterPut 函数。
 func New(ds datastore.Batching, afterPut hook.AfterPutFunc) datastore.Batching {
+	// 定义一个 afterBatch 函数，它在每次批处理操作之后调用。
 	afterBatch := func(b datastore.Batch, err error) (datastore.Batch, error) {
+		// 创建一个新的批处理对象，并在每次 Put 操作之后调用 afterPut 函数。
 		return batch.NewBatch(b, batch.WithAfterPut(batch.AfterPutFunc(afterPut))), err
 	}
 
+	// 返回一个新的代理数据存储，该数据存储在每次 Put 操作和批处理操作之后调用相应的回调函数。
 	return hook.NewBatching(ds, hook.WithAfterPut(afterPut), hook.WithAfterBatch(afterBatch))
 }
